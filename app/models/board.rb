c615c9bb5324cb646fa720fe8e298db0dc80577e
Gabriel Coronel <gabriel.c1901@gmail.com>
2023-08-03 15:58:22 -0300

    add validation for boards

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,6 @@
 class Board < ApplicationRecord
   has_many :lists, dependent: :destroy
   accepts_nested_attributes_for :lists, allow_destroy: true
+
+  validates :name, presence: true, uniqueness: true
 end
